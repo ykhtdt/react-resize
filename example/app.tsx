1c939ede527954073504c19d8baed67c96b9e441
jgpark-1 <jgpark7281@gmail.com>
2024-02-16 19:57:56 +0900

    feat(resize): add example page for element resize feature

--- example/app.tsx
+++ example/app.tsx
@@ -1,7 +1,19 @@
+import ResizeBox from "../src/resize-box";
+
 export default function App() {
   return (
     <div>
-      Example App Render
+      <ResizeBox
+        maxConstraints={[400, 600]}
+        top={200}
+        left={500}
+        width={440}
+        height={222}
+        resizeHandleAxis={["n", "ne", "e", "se", "s", "sw", "w", "nw"]}
+        className="inline-block border border-red-500"
+      >
+        Resize Box Example
+      </ResizeBox>
     </div>
   )
 }
