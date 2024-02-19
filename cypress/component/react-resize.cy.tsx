56c110a684c4598b18adacd15e512ca912aa1695
jgpark-1 <jgpark7281@gmail.com>
2024-02-20 04:38:06 +0900

    test(cypress): add cypress mount test

--- cypress/component/react-resize.cy.tsx
+++ cypress/component/react-resize.cy.tsx
@@ -1,7 +1,11 @@
 import { ResizeBox } from "../../lib/react-resize";
 
-describe('react-resize.cy.tsx', () => {
-  it('playground', () => {
-    cy.mount(<ResizeBox>resize-box</ResizeBox>);
+describe("react-resize.cy.tsx", () => {
+  const children = <div data-testid="resize-children">React Resize Children</div>
+
+  it("mounts", () => {
+    cy.mount(<ResizeBox>{children}</ResizeBox>);
+
+    cy.get("[data-cy=wrapper]").children().should("have.length.greaterThan", 0);
   })
 })
